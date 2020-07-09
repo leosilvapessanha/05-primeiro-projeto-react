93d6dbb3f0756aaeb49ffa99b5afb887bcd30ee0
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-09 19:11:53 -0300

    basic TS preparation is done. 1 bug

---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+//import { Title } from './styles'
+
+const Repository: React.FC = () => {
+return <h1>Explore repositórios no Github</h1>
+
+}
+
+export default Repository
