94bd1f294b2fc1e9a16a70a68401a03f0f087666
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-11 17:53:53 -0300

    api integration on frontend

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,30 +1,54 @@
-import React from 'react'
-
+import React, {useState, FormEvent} from 'react'
 import gitexplorer from "../../assets/gitexplorer.svg"
-
+import api from '../../services/api'
 import { Title, Form, Repositories } from './styles'
 import { FiChevronRight } from 'react-icons/fi'
 
+interface Repository{
+  full_name: string;
+  description: string;
+  owner: {
+    login: string,
+    avatar_url: string;
+  }
+}
+
 
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('')
+  const [repositories,setRepositories] = useState<Repository[]>([])
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void>{
+
+    event.preventDefault()
+
+    const response = await api.get<Repository>(`repos/${newRepo}`)
+
+    const repository = response.data
+
+    setRepositories([...repositories, repository])
+    setNewRepo('');
+
+  }
   return (
     <>
       <img src={gitexplorer} alt="GitHub Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form action="">
-        <input type="text" placeholder="digite o repositório aqui" />
+      <Form onSubmit={handleAddRepository}>
+        <input value={newRepo} onChange={(e) => setNewRepo(e.target.value)}
+          type="text" placeholder="digite o repositório aqui" />
         <button>Pesquisar</button>
       </Form>
       <Repositories>
-        <a href="">
-          <img src="https://avatars1.githubusercontent.com/u/43595244?s=460&u=cb080f7f07bb02dc5ced6942b26e871f26c48cb6&v=4" alt="Avatar Leonardo Pessanha" />
+        {repositories.map(repository=> (<a key={repository.full_name} href="test">
+          <img src={repository.owner.avatar_url} alt={repository.owner.login}/>
           <div>
-            <strong>leosilvapessanha/05-primeiro-projeto-react</strong>
-            <p>First project in React</p>
+            <strong>{repository.full_name}</strong>
+            <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20} />
-        </a>
+        </a>))}
+
       </Repositories>
     </>
   )
