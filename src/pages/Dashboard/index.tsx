a3cd947cc52695bcebaab1c58f3437a59f87a1d6
Leonardo Pessanha <leonardosilvapessanha@gmail.com>
2020-07-12 00:40:28 -0300

    finish, but with one error on repository index line 40

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,10 @@
-import React, { useState, FormEvent } from 'react'
+import React, { useState, useEffect, FormEvent } from 'react'
+import {Link} from 'react-router-dom'
 import gitexplorer from "../../assets/gitexplorer.svg"
 import api from '../../services/api'
 import { Title, Form, Repositories, Error } from './styles'
 import { FiChevronRight } from 'react-icons/fi'
-import { error } from 'console'
+
 
 interface Repository {
   full_name: string;
@@ -18,7 +19,18 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('')
   const [inputError, setInputError] = useState('')
-  const [repositories, setRepositories] = useState<Repository[]>([])
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem('@GithubExplorer:repositories')
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories)
+    }
+    else {
+      []
+    }
+  })
+
+  useEffect(() => { localStorage.setItem('@GithubExplorer:repositories', JSON.stringify(repositories),) }, [repositories])
+
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
 
     event.preventDefault()
@@ -55,14 +67,14 @@ const Dashboard: React.FC = () => {
       {inputError && <Error>{inputError}</Error>}
 
       <Repositories>
-        {repositories.map(repository => (<a key={repository.full_name} href="test">
+        {repositories.map(repository => (<Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
           <div>
             <strong>{repository.full_name}</strong>
             <p>{repository.description}</p>
           </div>
           <FiChevronRight size={20} />
-        </a>))}
+        </Link>))}
 
       </Repositories>
     </>
